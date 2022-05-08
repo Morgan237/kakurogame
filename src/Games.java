bb73864588b21d2fd592624f2fe3d3a19011fd04
Steve Morgan <stevekono77@gmail.com>
2022-05-08 01:37:08 +0100

    Touching Games class

--- src/Games.java
+++ src/Games.java
@@ -6,9 +6,9 @@ public class Games {
         this.setter = new String[]{g3, g4, g6, g7, g8, g9, g11, Integer.toString(h1), Integer.toString(h2)};
     }
 
-    public void initResponse(int f1,int f2,int f3,int f4,int f5,int f6,int f7){
-        this.answer = new int[]{f1,f2,f3,f4,f5,f6,f7};
-    }
+//    public void initResponse(int f1,int f2,int f3,int f4,int f5,int f6,int f7){
+//        this.answer = new int[]{f1,f2,f3,f4,f5,f6,f7};
+//    }
 
     public Games(){
 
